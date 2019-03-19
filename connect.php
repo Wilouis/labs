a3ac267b96bf268119ee417621f3c673881bde68
Wilfrid Louis <wilfrid_lo@yahoo.fr>
2019-03-19 00:38:29 -0400

    Adding informations about the server

--- connect.php
+++ connect.php
@@ -1,10 +1,11 @@
 <?php
- 		  
+
+/* In this file, I didn't want to use my server informations. So, you just have to import the database in your server with this same name and define the parameters below*/ 		  
 $connexion=mysql_connect ("server name","username","password")  
 or die("Erreur: Connexion avec le serveur echoue");
 
 mysql_select_db("base_247labs",$connexion)
 or die ("Erreur: Selection de la base de donnee");
-mysql_query("SET NAMES 'utf8'"); /* Permet d'afficher les lettres accentuees sans erreur */
+mysql_query("SET NAMES 'utf8'"); 
 
 ?>
